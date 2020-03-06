cac8f279859e3e73765b2e2c879e3619d264caf6
= <=>
2020-03-06 16:24:03 +0000

    started portfolio

---
+++ pages/countdown.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <b-container>
+      <h1>Countdown!</h1>
+      <h2>Round {{yourRound}}</h2>
+      <h2>Your score is {{yourScore}}</h2>
+      <p v-if="message">{{message}}</p>
+      <div v-if="!gameStarted">
+        <b-button @click="pickVowel">Pick a vowel</b-button>
+        <b-button @click="pickConsonant">Pick a consontant</b-button>
+      </div>
+      <h3 v-if="currentLetters.length">Your letters are:</h3>
+      <p class="mt-5">
+        <span
+          @click="selectLetter(index)"
+          class="letter"
+          v-for="(letter, index) in currentLetters"
+          :key="`a${letter}${index}`"
+        >{{letter}}</span>
+      </p>
+      <div class="mt-5" v-if="gameStarted">
+        <h3 v-if="yourWord.length">Your word is:</h3>
+        <p class="mt-5">
+          <span
+            @click="deselectLetter(index)"
+            class="letter"
+            v-for="(letter, index) in yourWord"
+            :key="`b${letter}${index}`"
+          >{{letter}}</span>
+        </p>
+        <h3 class="mt-5">{{timeLeft}} seconds left.</h3>
+        <b-button @click="submitWord" class="mt-5">Submit Word</b-button>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import _ from "lodash";
+export default {
+  data() {
+    return {
+      currentLetters: [],
+      yourWord: [],
+      vowels: "aeiou",
+      consonants: "bbbcccdddfffggghhjkllmmnnpppqrrrssstttvwxyz",
+      timeLeft: 30,
+      timerInterval: null,
+      dictionary: ["dogs"],
+      gameStarted: false,
+      yourScore: 0,
+      yourRound: 1,
+      message:
+        "Welcome to countdown! Choose consonant or vowels. Click on letters to select or deselect. Click submit word when fininshed!"
+    };
+  },
+  methods: {
+    pickVowel() {
+      this.currentLetters.push(_.sample(this.vowels.split("")));
+    },
+    pickConsonant() {
+      this.currentLetters.push(_.sample(this.consonants.split("")));
+    },
+    selectLetter(index) {
+      this.gameStarted &&
+        this.yourWord.push(this.currentLetters.splice(index, 1)[0]);
+    },
+    deselectLetter(index) {
+      this.currentLetters.push(this.yourWord.splice(index, 1)[0]);
+    },
+    submitWord() {
+      if (this.dictionary.includes(this.yourWord.join())) {
+        const score = this.yourWord.length;
+        this.yourScore += score;
+        this.message = `Well done, you scored ${score} points`;
+      } else {
+        this.message = "Incorrect word. You scored 0 points.";
+      }
+      this.currentLetters = [];
+      this.yourWord = [];
+      this.gameStarted = false;
+      this.yourRound++;
+      clearInterval(this.timerInterval);
+    },
+    startTimer() {
+      this.timeLeft = 30;
+      this.timerInterval = setInterval(() => this.timeLeft--, 1000);
+    }
+  },
+  computed: {},
+  watch: {
+    currentLetters() {
+      if (this.currentLetters.length === 10) {
+        this.gameStarted = true;
+        this.startTimer();
+      }
+    },
+    timeLeft() {
+      if (this.timeLeft >= 0) {
+        this.submitWord();
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.letter {
+  padding: 30px;
+  margin: 10px;
+  font-size: 30px;
+  background-color: rgb(22, 81, 184);
+  color: white;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <b-container>
+      <h1>Otto's Portfolio</h1>
+      <b-card-group deck>
+        <div v-for="project in projects" :key="project.title">
+          <b-card :title="project.title" :sub-title="project.description">
+            <b-button :to="project.title">Go to project</b-button>
+            <template v-slot:footer>
+              <div v-for="tag in project.tags" :key="tag">
+                <b-badge>{{tag}}</b-badge>
+              </div>
+            </template>
+          </b-card>
+        </div>
+      </b-card-group>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: "Countdown",
+          description: "A game of wits",
+          tags: ["algorithms", "stuff"]
+        },
+        { title: "Timer", description: "test", tags: [] },
+        { title: "Thingy", description: "test", tags: [] },
+        { title: "Stuff", description: "test", tags: [] }
+      ]
+    };
+  },
+  methods: {},
+  computed: {}
+};
+</script>
+
+<style scoped>
+</style>
