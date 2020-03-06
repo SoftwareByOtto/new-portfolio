2c9a550797eb07467171d48013263a5a92c979ad
= <=>
2020-03-06 17:30:26 +0000

    improved styling, added correct answers

--- pages/countdown.vue
+++ pages/countdown.vue
@@ -1,36 +1,44 @@
 <template>
   <div>
     <b-container>
-      <h1>Countdown!</h1>
-      <h2>Round {{ yourRound }}</h2>
-      <h2>Your score is {{ yourScore }}</h2>
+      <h1>
+        COUNT
+        <img
+          src="https://vignette.wikia.nocookie.net/logopedia/images/b/ba/Countdown_2012.png/revision/latest?cb=20120109211433"
+          alt=""
+          width="100px"
+        />
+        DOWN
+      </h1>
+      <h2>Round {{ yourRound }}, Your score is {{ yourScore }}</h2>
       <p v-if="message">{{ message }}</p>
       <div v-if="!gameStarted">
         <b-button @click="pickVowel">Pick a vowel</b-button>
         <b-button @click="pickConsonant">Pick a consontant</b-button>
       </div>
       <h3 v-if="currentLetters.length">Your letters are:</h3>
-      <p class="mt-5">
-        <span
+      <p class="mt-3">
+        <b-button
           @click="selectLetter(index)"
           class="letter"
           v-for="(letter, index) in currentLetters"
           :key="`a${letter}${index}`"
-          >{{ letter }}</span
+          >{{ letter }}</b-button
         >
       </p>
-      <div class="mt-5" v-if="gameStarted">
+      <div class="mt-3" v-if="gameStarted">
         <h3 v-if="yourWord.length">Your word is:</h3>
-        <p class="mt-5">
-          <span
+        <p class="mt-3">
+          <b-button
             @click="deselectLetter(index)"
             class="letter"
             v-for="(letter, index) in yourWord"
             :key="`b${letter}${index}`"
-            >{{ letter }}</span
+            >{{ letter }}</b-button
           >
         </p>
-        <h3 class="mt-5">{{ timeLeft }} seconds left.</h3>
+        <hr />
+        <h3 class="mt-3">{{ timeLeft }} seconds left.</h3>
         <b-button @click="submitWord">Submit Word</b-button>
       </div>
     </b-container>
@@ -40,6 +48,7 @@
 <script>
 import _ from "lodash";
 import words from "@/static/words.json";
+import scrabble from "@nosweat/scrabble";
 export default {
   data() {
     return {
@@ -53,6 +62,7 @@ export default {
       gameStarted: false,
       yourScore: 0,
       yourRound: 1,
+      answers: [],
       message:
         "Welcome to countdown! Choose consonant or vowels. Click on letters to select or deselect. Click submit word when fininshed!"
     };
@@ -75,10 +85,13 @@ export default {
       if (this.dictionary.includes(this.yourWord.join(""))) {
         const score = this.yourWord.length;
         this.yourScore += score;
-        this.message = `Well done, you scored ${score} points`;
+        this.message = `Well done, you scored ${score} points!`;
       } else {
         this.message = "Incorrect word. You scored 0 points.";
       }
+      this.message = this.message.concat(
+        ` You could have said ${this.answers[0]}, ${this.answers[1]} or ${this.answers[2]}.`
+      );
       this.currentLetters = [];
       this.yourWord = [];
       this.gameStarted = false;
@@ -105,6 +118,9 @@ export default {
     gameStarted() {
       if (this.gameStarted) {
         this.startTimer();
+        this.answers = scrabble
+          .find(this.currentLetters.join(""))
+          .sort((a, b) => b.length - a.length);
       } else {
         clearInterval(this.timerInterval);
       }
@@ -115,10 +131,16 @@ export default {
 
 <style scoped>
 .letter {
-  padding: 30px;
+  padding: 15px;
   margin: 10px;
   font-size: 30px;
   background-color: rgb(22, 81, 184);
   color: white;
+  box-shadow: 10px 10px 5px grey;
+  text-transform: uppercase;
+  font-weight: bold;
+  text-decoration-style: none;
+  display: inline-block;
+  width: 75px;
 }
 </style>
